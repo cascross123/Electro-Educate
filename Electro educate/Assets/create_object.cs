8e9f7917d2b393bbe9514beac723ed76df513913
Nick Jennett <nick.jennett@probablyrational.com>
2017-04-01 16:44:05 +0100

    Added ability to create object programmatically

---
+++ Electro educate/Assets/create_object.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class create_object : MonoBehaviour {
+	public GameObject Prefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void OnMouseDown() {
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if (Physics.Raycast(ray))
+				Instantiate(Prefab, transform.position, transform.rotation);
+		
+	
+	}
+}
